da6fe0a863f337a6a003f6abf1ca7e8b298a12be
takizawa897 <pc-524@st-ove.co.jp>
2025-09-24 14:26:04 +0900

    2日目の提出です。

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,50 +1,70 @@
-import { useState } from 'react'
-type Todo ={
+import { useState } from 'react';
+
+type Todo = {
   value: string;
-  readonly id : number;
+  readonly id:number;
 };
 
-export const App = () =>{
-  const [text,setText]=useState('');
-  const [todos,setTodos]=useState<Todo[]>([]);
+export const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
-  const handleSubmit = () =>{
-    if(!text) return;
-  
 
-  const newTodo:Todo ={
-    value:text,
-  }
+ const handleEdit = (id:number,value:string)=>{
+  setTodos((todos)=>{
+    const newTodos = todos.map((todo)=>{
+      if(todo.id === id){
+        todo.value = value;
+      }
+      console.log(todo);
+      return todo;
+    });
+    console.log('===Original todos===');
+    todos.map((todo)=>{
+      console.log('id:${todo.id}, value: ${todo.value}');
+  });
+    return newTodos;
+  });
+ };
+  const handleSubmit = () => {
+    if (!text) return;
 
-  setTodos((todos)=>[newTodo,...todos]);
-  setText('');
-};
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    };
 
-  return(
+    setTodos((todos) => [newTodo, ...todos]);
+    setText('');
+  };
+
+  return (
     <div>
-      <form onSubmit={(e) => { e.preventDefault()
-       handleSubmit(); 
-      }}
-      >
-        <input type="text" 
-        value={text} 
-        //onChange={(e)=>setText(e.target.value)}
-        onChange={(e)=>handleChange(e)}
-        />
-        <input type="submit"
-        //多分ここが、登録
-        value="追加" onSubmit={handleSubmit}
-        />
-      </form>
-      <ul>
-        {todos.map((todo)=>{
-          return <li>{todo.value}</li>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <input type="text" value={text} onChange={(e) => handleChange(e)} />
+          <input type="submit" value="追加" onSubmit={handleSubmit} />
+        </form>
+        <ul>  
+          {todos.map((todo)=>{
+            return(
+            <li key={todo.id}>
+              <input
+              type="text"
+              value={todo.value}
+              onChange={(e)=>handleEdit(todo.id,e.target.value)}
+              />
+            </li>
+          );
         })}
       </ul>
-      {/* //<p>{text}</p> 2025年9月22日12時44分取り敢えず必要なくなったから消す*/}
     </div>
   );
 };
